703ca97fed900b159361654291eba6b17d240a7a
serr <sc7227484@gmail.com>
2025-10-16 12:38:02 +0300

    add todo for ticker

--- utils/ticker.go
+++ utils/ticker.go
@@ -4,6 +4,7 @@ package utils
 import "time"
 
 //  NOTE: Ticker
+//  TODO: Написать тесты для тикера
 // Run func at the specified interval in a separate goroutine
 // Returns the channel for stopping the ticker
 
