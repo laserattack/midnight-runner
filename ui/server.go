ac30ce906d57dc08b2eb8a1d6a26a34c40d57c9c
serr <sc7227484@gmail.com>
2025-10-18 20:39:19 +0300

    quick update

--- ui/server.go
+++ ui/server.go
@@ -12,8 +12,11 @@ func CreateWebServer(port string, slogLogger *slog.Logger) *http.Server {
 		Addr: ":" + port,
 		Handler: http.HandlerFunc(
 			func(w http.ResponseWriter, r *http.Request) {
-				_, _ = fmt.Fprintf(w, "Hello World!\n")
-				_, _ = fmt.Fprintf(w, "Job Scheduler is running...\n")
+				_, err := fmt.Fprintf(w, "Hello World!\n")
+				if err != nil {
+					slogLogger.Error("failed to write response", "error", err)
+					return
+				}
 			}),
 	}
 }
