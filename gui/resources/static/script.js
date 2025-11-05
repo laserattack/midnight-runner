a40a0e101ee8c61151375b97bd1501c2472a00c9
serr <sc7227484@gmail.com>
2025-11-05 13:55:47 +0300

    quick update

--- gui/resources/static/script.js
+++ gui/resources/static/script.js
@@ -33,7 +33,7 @@ function formatTimestamp(timestamp) {
         hour: '2-digit',
         minute: '2-digit',
         second: '2-digit'
-    }).replace(/,/g, '');
+    });
 }
 
 function updateJobsTable(data) {
