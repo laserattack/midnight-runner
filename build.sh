425e9fcb55b12d8d8a04d06fadc4f559dbf4c17b
serr <sc7227484@gmail.com>
2025-11-08 19:18:55 +0300

    quick update

--- build.sh
+++ build.sh
@@ -3,15 +3,16 @@
 set -e
 
 BINARY_NAME="mr"
+DIR_FOR_BINS=".bin"
 
 echo "Building for Linux (amd64)..."
-GOOS=linux GOARCH=amd64 CGO_ENABLED=0 go build -o "${BINARY_NAME}-linux" .
+GOOS=linux GOARCH=amd64 CGO_ENABLED=0 go build -o "${DIR_FOR_BINS}/${BINARY_NAME}-linux-amd64" .
 echo "OK"
 
 echo "Building for Windows (amd64)..."
-GOOS=windows GOARCH=amd64 CGO_ENABLED=0 go build -o "${BINARY_NAME}.exe" .
+GOOS=windows GOARCH=amd64 CGO_ENABLED=0 go build -o "${DIR_FOR_BINS}/${BINARY_NAME}-windows-amd64.exe" .
 echo "OK"
 
 echo "Done!"
 echo "Binaries:"
-ls -l "${BINARY_NAME}-linux" "${BINARY_NAME}.exe"
+ls -l ./"${DIR_FOR_BINS}"/*
