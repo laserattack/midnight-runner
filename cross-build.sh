b16b3fe38a239ebb11e3d55667f2cbe08e7ec010
serr <sc7227484@gmail.com>
2025-11-08 19:20:47 +0300

    quick update

--- cross-build.sh
+++ cross-build.sh
@@ -8,7 +8,6 @@ DIR_FOR_BINS=".bin"
 echo "Building for Linux (amd64)..."
 GOOS=linux GOARCH=amd64 CGO_ENABLED=0 go build -o "${DIR_FOR_BINS}/${BINARY_NAME}-linux-amd64" .
 echo "OK"
-
 echo "Building for Windows (amd64)..."
 GOOS=windows GOARCH=amd64 CGO_ENABLED=0 go build -o "${DIR_FOR_BINS}/${BINARY_NAME}-windows-amd64.exe" .
 echo "OK"
